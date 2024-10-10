87417f55691f240d67b07ebcba1b698365f2f124
BradleyCurtis <2309516@students.ucreative.ac.uk>
2024-10-10 11:39:26 +0100

    Creation of the dice rolling mechanic and adding it into separate text boxes.

---
+++ RPG Dice Game/Assets/Scripts/Dice/DiceRoller.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    
+    public TMP_InputField dice6ResultText;
+    public TMP_InputField dice12ResultText;
+    public TMP_InputField dice20ResultText;
+
+    public Button roll;
+
+    
+    void Start()
+    {
+        roll.onClick.AddListener(RollDice);
+    }
+
+    
+    public void RollDice()
+    {
+        int dice6Result = RollDice(6);
+        int dice12Result = RollDice(12);
+        int dice20Result = RollDice(20);
+
+        dice6ResultText.text = "6 Sided Dice Roll: " + dice6Result;
+        dice12ResultText.text = "12 Sided Dice Roll: " + dice12Result;
+        dice20ResultText.text = "20 Sided Dice Roll: " + dice20Result;
+    }
+
+    private int RollDice(int sides)
+    {
+        return Random.Range(1, sides+1);
+    }
+}
+
